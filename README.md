001fa6b6e7a401afe90e2f810ebeade340d134cf
AleixSaPa <sapafamilia@gmail.com>
2024-07-18 19:58:43 +0200

    Fer Las Bases De La Web

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # EscolaFenCreixa
-
-Es Par La Escola Par Tinder Una We
+## Qui A Fet La Web
+Es Par La Escola Par Tinder Una Web
+Pogrmada Par L'aleix El Alubne Que A Pogramat Aquesta Web
---
+++ ScriptArchivoJson.js
@@ -0,0 +1,62 @@
+var Eventos =
+    [
+
+        {
+            title: 'Aniversari Esteve',
+            start: '2025-01-08',
+            mes: 'Setembre',
+            year: '2024'
+        },
+
+
+
+        {
+            title: 'Aniversari Vanessa',
+            start: '2025-01-23',
+            mes: 'Setembre',
+            year: '2024'
+        },
+        {
+            title: 'Aniversari Noemi',
+            start: '2025-01-26',
+            mes: 'Setembre',
+            year: '2024'
+        },
+        {
+            title: 'Aniversari Noam',
+            start: '2025-06-22',
+            mes: 'Setembre',
+            year: '2024'
+        },
+        {
+            title: 'Aniversari Lois',
+            start: '2025-07-04',
+            mes: 'Setembre',
+            year: '2024'
+        },
+        {
+            title: 'Aniversari Aleix',
+            start: '2024-09-01',
+            mes: 'Setembre',
+            year: '2024'
+        },
+        {
+            title: 'Cumple Pilar',
+            start: '2024-10-12',
+            mes: 'octubre',
+            year: '2024'
+        },
+        {
+            title: 'Aniversari Mat',
+            start: '2024-12-30',
+            mes: 'Desembre',
+            year: '2024'
+        },
+        {
+            title: 'Cumancem Clases',
+            start: '2024-09-09',
+            mes: 'Setembre',
+            year: '2024'
+        },
+    ];
+//2025-06-17
