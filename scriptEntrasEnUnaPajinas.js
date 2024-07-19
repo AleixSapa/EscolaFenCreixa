0759fb84128539c20dadaceae3006e82b26a2229
Esanpons <ecs200@hotmail.com>
2024-07-19 20:09:07 +0200

    Extewnciones

--- scriptEntrasEnUnaPajinas.js
+++
@@ -1,9 +0,0 @@
-function Calendari(params) {
-    alert('Benbingut al calandari de la escola fem creixa')
-}
-function Horari_Semanal(params) {
-    alert('Vas a veure quan tindras que traballa')
-}
-function Alubnes_I_Pufasos(params) {
-    alert('Vas A Beure Qui Es Alubne i qui es mestre')
-}
